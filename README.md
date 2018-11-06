ffc9cdb6f13f09887838364b1e7e386b6af76f7a
Heldridge, Jeffrey M <Jeffrey.Heldridge@tdameritrade.com>
2018-11-06 08:49:34 -0600

    Inital Commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # restli_explorer
 Trying out the rest.li services
+
+I built this repo with VS Code and the  Gradle Language Support Extension.  This was built off of the rest.li walk through tutorial located https://github.com/linkedin/rest.li/wiki/Quickstart:-A-Tutorial-Introduction-to-Rest.li#creating-a-client
+
---
+++ server/src/main/java/com/model/mapmarker/impl/MapMarkerResource.java
@@ -0,0 +1,30 @@
+package com.model.mapmarker.impl;
+import com.linkedin.restli.server.annotations.RestLiCollection;
+import com.linkedin.restli.server.resources.CollectionResourceTemplate;
+import com.model.mapmarker.MapMarker;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestLiCollection(name = "mapmarker", namespace = "com.model.mapmarker")
+public class MapMarkerResource extends CollectionResourceTemplate<Long, MapMarker> {
+
+  // In-memory store for the fortunes
+  static Map<Long, String> markers = new HashMap<Long, String, String, String>();
+  static {
+    markers.put(1L, "here", "45.2222", "-45.3333");
+    markers.put(2L, "There's no time like the present.");
+    markers.put(3L, "Don't worry, be happy.");
+  }
+
+  @Override
+  public MapMarker get(Long key) {
+    // Retrieve the requested fortune
+    String markers = markers.get(key);
+    if (marker == null) {
+      marker = "No marker for that id: " + key;
+    }
+
+    // return an object that represents the fortune cookie
+    return new MapMarker().setMapMarker(mapmarker);
+  }
+}
